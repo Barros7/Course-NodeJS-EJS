fc5e4d0c4103e0f02eac2e94743a9952ceff7da0
Barros7 <josebarros412@gmail.com>
2022-02-07 04:33:53 +0000

    forEach with EJS

--- server.js
+++ server.js
@@ -5,11 +5,44 @@ const port = 8080;
 app.set("view engine", "ejs"); //definir o tipo de renderização como ejs
 
 app.get("/", (req, res) => { //cria uma rota raiz e renderiza o index
-    res.render("pages/index");
+    const adjetives = [
+        {
+            title:'B',
+            message: 'allsy'
+        },
+        {
+            title: 'A',
+            message: 'wesome'
+        },
+        {
+            title: 'R',
+            message: 'adiant'
+        },
+        {
+            title: 'R',
+            message: 'evolutionary'
+        },
+        {
+            title: 'O',
+            message: 'vermodest'
+        },
+        {
+            title: 'S',
+            message: 'mart'
+        }
+    ]
+    res.render("pages/index", {
+        qualitys: adjetives,
+        title: 'Node + EJS',
+        paragraph: 'Learning the work with EJS file'
+    });
 });
 
 app.get("/about", (req, res) => {
-    res.render("pages/about");
+    res.render("pages/about", {
+        title: 'About me',
+        paragraph: 'I´m José Bongo, Web Informátic student'
+    });
 });
 
 app.listen(port, () => {
